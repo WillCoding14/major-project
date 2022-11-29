cbb7a25d8df44464998fbd7a2bd9916a9aba9eed
Will <william.hlady@gmail.com>
2022-11-29 12:14:35 -0600

    Basic collision working with classes

--- sketch.js
+++ sketch.js
@@ -2,9 +2,6 @@
 // let name = Will;
 // let date = November 17, 2022;
 //}
-
-let hit = false;
-
 class Player{
   constructor(x, y, w, h){
     this.x = x;
@@ -16,34 +13,28 @@ class Player{
   display(){
     rect(this.x, this.y, this.w, this.h);
   }
-}
 
-class Platform{
-  constructor(x, y, w, h){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-  }
-
-  display(){
-    rect(this.x, this.y, this.w, this.h);
+  update(){
+    this.x = mouseX;
+    this.y = mouseY;
   }
 }
 
+let hit = false;
+let player;
 
-
-function setup() {
+function setup(){
   createCanvas(windowWidth, windowHeight);
+  player = new Player(mouseX, mouseY, 100, 100);
 }
-
 
 function draw() {
   background(255);
-  let player = new Player(0, height-50, width, 50);
-  let newPlatform = new Platform(mouseX, mouseY, 50, 75);
+  rect(200, 200, 100, 150);
+  player.display();
+  player.update();
 
-  hit = collideRectRect(newPlatform.x, newPlatform.y, newPlatform.w, newPlatform.h, player.x, player.y, player.w, player.h);
+  hit = collideRectRect(200, 200, 100, 150, player.x, player.y, player.w, player.h);
 
   stroke(hit ? color("red") : 0);
   print("colliding?", hit);
