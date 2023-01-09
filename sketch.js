26cbff0723db1cf3fbc03bdc691da4fdc0250cc8
Will <william.hlady@gmail.com>
2023-01-09 11:18:16 -0600

    Creating player class

--- sketch.js
+++ sketch.js
@@ -4,19 +4,26 @@ function setup() {
   new Canvas(windowWidth, windowHeight);
   world.gravity.y = 15;
 
-  player = new Sprite(width/2, 300, 100, 100, "dynamic");
   platform = new Sprite(width/2, 400, width, 50, "static");
 
 }
 
 function draw() {
   clear();
+}
 
+function createPlayer(){
+  player = new Sprite(width/2, 300, 100, 100, "dynamic");
+
+}
+
+function playerJump(){
   if (player.colliding(platform)){
     if (kb.presses("up")){
       player.collider = "dynamic";
       player.vel.y = -9;
     }
   }
+
 }
 
