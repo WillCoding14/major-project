80a37351cbb200d15c23e43bb5017e4e81b977bb
Will <william.hlady@gmail.com>
2023-01-10 11:05:13 -0600

    Added class for player

--- sketch.js
+++ sketch.js
@@ -1,21 +1,33 @@
-let player, platform;
+let platform;
+let player;
+
+class p{
+  constructor(x, y, w, h, col){
+    this.x = width/2;
+    this.y = 300;
+    this.w = 100;
+    this.h = 100;
+    this.col = "dynamic";
+  }
+  display(){
+    player = new Sprite(this.x, this.y, this.w, this.h, this.col);
+  }
+  jump(){
+    
+  }
+}
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
   world.gravity.y = 15;
 
   platform = new Sprite(width/2, 400, width, 50, "static");
-
 }
 
 function draw() {
   clear();
 }
 
-function createPlayer(){
-  player = new Sprite(width/2, 300, 100, 100, "dynamic");
-
-}
 
 function playerJump(){
   if (player.colliding(platform)){
