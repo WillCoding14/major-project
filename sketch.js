a97a3d5a7962cf45f314a4a1d1ffeb5825417e5f
Will <william.hlady@gmail.com>
2023-01-17 12:11:10 -0600

    Added start screen and different states

--- sketch.js
+++ sketch.js
@@ -1,37 +1,71 @@
 let player;
 let ground;
 let platforms;
-let jumpcharge = 0;
+let button;
+let backgroundimage;
 
+
+let jumpcharge = 0;
+let state = "start";
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ground = new Sprite(width/2, height/2, width - 100, 30);
-  player = new Sprite(width/2, 300, 100, 100);
-  world.gravity.y = 15;
+  //player and platforms creation
+  player = new Sprite(width/2, 300, 100, 100, "k");
+  ground = new Sprite(width/2, height/2, width - 100, 30, "static");
   ground.collider = "static";
   player.bounciness = 0;
+
+  //world setup
+  world.gravity.y = 15;
+
+  //start screen
+  backgroundimage = new Sprite();
+  backgroundimage.img = "assets/startbackgroundimg.png";
+  backgroundimage.collider = "static";
+  backgroundimage.img.scale.x = 2;
+  backgroundimage.img.scale.y = 2.2;
+  
+
+  button = new Sprite(width/2, height/2, 300, 100,"static");
+  button.img = "assets/startbuttonimg.png";
+
+  backgroundimage = (0, 0, width, height);
 }
 
 function draw() {
-  background(0);
+  if (state === "start"){
+    background(0);
+    player.visible = false;
+    ground.visible = false;
+    clear();
+  }
+  else if (state === "main"){
+    background(0);
+    move();
+    
+    if (kb.presses("space")){
+      jumpLogic();
+    }
+    else{
+      player.vel.x = 0;
+    }
+  }
+  clear();
+}
+
+
+function jumpLogic() {
+  player.velocity.y = -5;
+  player.velocity.x = -5;
+}
+
+function move(){
   if (kb.pressing("left")) {
     player.vel.x = -5;
   }
   else if (kb.pressing("right")) {
     player.vel.x = 5;
   }
-  if (kb.presses("space")){
-    jump();
-  }
-  else{
-    player.vel.x = 0;
-  }
-  clear();
-}
-
-
-function jump() {
-  player.velocity.y = -5;
 }
