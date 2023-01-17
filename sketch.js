4caaa25bc0d360baeba342fa0332366d99cddaec
Will <william.hlady@gmail.com>
2023-01-17 11:08:21 -0600

    forgot to push from yesterday

--- sketch.js
+++ sketch.js
@@ -1,23 +1,29 @@
 let player;
 let ground;
 let platforms;
+let jumpcharge = 0;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ground = new Sprite(width/2, height/2, width - 100, 30);
   player = new Sprite(width/2, 300, 100, 100);
-  ground = new Sprite(width/2, height/2, width, 30);
-  world.gravity.y = 10;
+  world.gravity.y = 15;
   ground.collider = "static";
-
+  player.bounciness = 0;
 }
 
 function draw() {
   background(0);
   if (kb.pressing("left")) {
-    player.vel.x = -3;
+    player.vel.x = -5;
   }
   else if (kb.pressing("right")) {
-    player.vel.x = 3;
+    player.vel.x = 5;
+  }
+  if (kb.presses("space")){
+    jump();
   }
   else{
     player.vel.x = 0;
@@ -25,6 +31,7 @@ function draw() {
   clear();
 }
 
+
 function jump() {
-  player.velocity.y = -10;
+  player.velocity.y = -5;
 }
