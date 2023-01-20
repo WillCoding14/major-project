78179f299241dd11c0944ecb3426eef5a958361f
Will <william.hlady@gmail.com>
2023-01-20 11:13:00 -0600

    attempted to figure out rest of start screen

--- sketch.js
+++ sketch.js
@@ -10,12 +10,11 @@ let state = "start";
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(2050, 1000);
   //player and platforms creation
   player = new Sprite(width/2, 300, 100, 100, "k");
   ground = new Sprite(width/2, height/2, width - 100, 30, "static");
   ground.collider = "static";
-  player.bounciness = 0;
 
   //world setup
   world.gravity.y = 15;
@@ -24,24 +23,27 @@ function setup() {
   backgroundimage = new Sprite(width/2, height/2, width, height, "static");
   backgroundimage.color = "black";
   
-
   button = new Sprite(width/2, height/2, 300, 100,"static");
   button.img = "assets/startbuttonimg.png";
+
+  // button.img.scale.x = 0.5;
+  // button.img.scale.y = 0.5;
 
   backgroundimage = (0, 0, width, height);
 }
 
 function draw() {
   if (state === "start"){
-    background(0);
-    player.visible = false;
-    ground.visible = false;
-    clear();
+    mainScreen();
   }
-  else if (state === "main"){
+  if (state === "main"){
     background(0);
     move();
-    
+    button.visible = false;
+    backgroundimage.visible = false;
+
+    player.visible = true;
+    ground.visible = true;
     if (kb.presses("space")){
       jumpLogic();
     }
@@ -52,6 +54,21 @@ function draw() {
   clear();
 }
 
+function mainScreen(){
+  background(0);
+  player.visible = false;
+  ground.visible = false;
+  // if (button.mouse.hovering()){
+  //   button.img = "assets/startbuttonhoverimg.png";
+  // }
+  // else{
+  //   button.img = "assets/startbuttonimg.png";
+  // }
+  if (button.mouse.presses){
+    state = "main";
+  }
+  clear();
+}
 
 function jumpLogic() {
   player.velocity.y = -5;
