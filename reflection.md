8d46ceabb2b02c33fb611fe2ac1e39bb95c60260
Will <william.hlady@gmail.com>
2023-01-24 11:15:55 -0600

    Fixed merging issues

--- reflection.md
+++ reflection.md
@@ -1,20 +1,10 @@
 // Problems/Challenges that occured
-
 In this project, it felt like just about everything that could go wrong did indeed go wrong. At first I attempted to use the p5.js collision library and then build my very own physics engine. I put a few hours into trying to make this work but soon realized the difficulty of creating such a thing would be too difficult based on the amount of time I had to work on the project outside of school.
-
 I soon moved on to using p5.play instead of the p5 collision library and at first things went alot smoother. I was picking up how it worked pretty quick and decided it was time to try putting some of it into practice. Unfortunately, I was really struggling with the new syntax and put my focus towards the wrong things. I felt like I was in sort of a loop and ended up "wasting" 10+ hours just writing and rewriting all to end up at the same spot.
-
 Another challenge that occurred was I didn't have a whole lot of opportunities to work on the project outside of school because of my job and some mental health issues that I was battling. This often meant that I would sort of lose track in between work periods and especially in between weekends and come back not remembering everything and having to realearn.
-
 The final sort of challenge that I was facing was more of a mental one. Towards the end of the project my frustration started getting the better of me and working on the project started to become more of a burden than a fun challenge.
-
-
 //Things I would do differently
-
 One of the main reasons that I think I struggled so much was I was putting my efforts into the wrong parts of the project. At the start I used too much of my time trying to figure out spritesheets and the looks of the game and should have spent more time working on the mechanics.
-
 Another thing I should have done is start with p5.play to begin with, this would have made it easier from the get go and less time would have been wasted.
-
 The final main thing I should have done differently is reach out for help more. Throughout most of the project, I convinced myself that it was important to do it all myself even when I was struggling towards the end.
-
 //Completion
--- sketch.js
+++ sketch.js
@@ -5,97 +5,74 @@ let state = "main";
 let playerSize = 30;
 let ready = false;
 let sizzle;
-
 //Loading sounds
 function preload(){
   sizzle = "Assets/sizzle.mp3";
 }
-
 function setup() {
   //World setup
   new Canvas(windowWidth - 20, windowHeight - 20);
   world.gravity.y = 13;
-
   // Sprite setup
   player = new Sprite(width/16, 300, playerSize);
   player.color = "black";
   player.layer = 2;
   /// player.img = "Assets/redball.png";
-
   lava = new Sprite(width/2, height/2 + 400, width, 300, "static");
   lava.img = "Assets/lavaimg.png";
   lava.img.scale.x = 3;
   lava.img.scale.y = 2;
-
   backgroundImg = new Sprite(width/2, height/2, width, height, "none");
   backgroundImg.layer = 0;
   backgroundImg.img = "Assets/cavebackground.png";
   backgroundImg.scale.x = 2;
   backgroundImg.scale.y = 2;
-
   finishline = new Sprite(width - 34, height/2, 40, height, "none");
   finishline.img = "Assets/finishline.png";
   finishline.img.rotation = 90;
   finishline.collider = "static";
-
   //Groups Setup
   platforms = new Group();
   orbs = new Group();
-
   //Platforms setup
   for (let i = 0; i < 7; i++){
     let platform = new platforms.Sprite();
   }
-
   platforms.y = height/2;
   platforms.collider = "static";
   platforms.height = 5;
   platforms.width = 30;
   // platforms.img = "Assets/platform.png";
-
   platforms[0].x = width/16;
-
   platforms[1].x = width/8;
   platforms[1].y = height/2 + 50;
-
   platforms[2].x = width / 4;
   platforms[2].y = height/2 - 25;
-
   platforms[3].x = width/3 + 40;
   platforms[3].y = height/2 - 25;
-
   platforms[4].x = width/2 + 400;
   platforms[4].y = height/2 + 150;
-
   platforms[5].x = width/2 + 600;
   platforms[5].y = height/2 + 150;
-
   //Jump recharge (orbs) setup
   for (let i = 0; i < 5; i++){
     let orb = new orbs.Sprite();
   }
-
   orbs.width = 10;
   orbs.height = 10;
   orbs.collider = "static";
   orbs.img = "Assets/orb.png";
-
   orbs[0].x = width/6 + 20;
   orbs[0].y = height/2;
-
   orbs[1].x = width/2 - 100;
   orbs[1].y = height/2 + 50;
-
   orbs[2].x = width/2 + 100;
   orbs[2].y = height/2 - 50;
-
   orbs[3].x = width/2 + 250;
   orbs[3].y = height/2 - 75;
-
   orbs[4].x = width/2 + 750;
   orbs[4].y = height/2 + 100;
 }
-
 function draw(){
   //Start screen state
   if (state === "start"){
@@ -112,13 +89,11 @@ function draw(){
   else if (state === "death"){
     deathScreen();
   }
-
   else if (state === "win"){
     lava.remove();
   }
   clear();
 }
-
 function playerMove(){
   //Move left
   if (kb.pressing("left")){
@@ -132,44 +107,37 @@ function playerMove(){
     player.vel.x = 0;
   }
 }
-
 function checkReady(){
   if (player.collides(platforms) || player.overlaps(orbs)){
     ready = true;
     player.color = "green";
   }
 }
-
 function playerJump(){
   if (ready === true){
     if (kb.presses("space")){
       player.vel.y = -5;
-      ready = true;
+      ready = false;
       player.color = "black";
     }
   }
 }
-
 function deathCheck(){
   if (player.collides(lava)){
     player.remove();
     state = "death";
   }
 }
-
 function winCheck(){
   if (player.overlaps(finishline)){
     player.remove();
     state = "win";
   }
 }
-
 // function toggleReady(){
 //   ready === !ready;
 // }
-
 function startScreen(){
 }
-
 function deathScreen(){
 }
